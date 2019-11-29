43cfb571fd6c48a08cb19f1540a700cbd6f2860b
kymarek <kymarek@mail.ru>
2019-11-29 12:55:24 +0400

    Header
    Only Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component{
+    render(){
+    return (
+        <div className='golova'>
+            <div className="shapka">
+            <div className="Login">
+            <div className="Autoriz">Авторизация:</div>
+                <div className="LoginUch">Учреждение</div>
+                <div className="LoginRab">Работодатель</div>
+                </div>    
+            </div>
+    </div> 
+    );
+    }
+}
+
+export default Header;
