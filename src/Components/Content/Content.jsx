a21397d0072a9e784debb192bf7c37bb05cc580c
kymarek <kymarek@mail.ru>
2019-11-29 14:20:25 +0400

    FInal

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Content.css';
+import photo from './photo.jpg';
 
 class Content extends React.Component{
     render(){
     return (
         <div className='Content'>
           <div className="block">
-              <div className="Photo"></div>
+              <div className="Photo"><img className="Photo" src={photo}/></div>
+              <div className="FIO">Иванова Елена Ивановна</div>
           </div>  
     </div> 
     );
