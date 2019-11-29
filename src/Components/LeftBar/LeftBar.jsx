462624ffd509aede3f6bd0b0d235286f88d5bd72
kymarek <kymarek@mail.ru>
2019-11-29 13:24:00 +0400

    Second

--- src/Components/LeftBar/LeftBar.jsx
+++ src/Components/LeftBar/LeftBar.jsx
@@ -5,7 +5,17 @@ class LeftBar extends React.Component{
     render(){
     return (
         <div className='LeftBar'>
-            
+            <h1 className="Zagolovok">Навигация</h1>
+        <ul type="disk" className="Spisok">
+            <li>Элемент 1</li>
+            <li>Элемент 1</li>
+            <li>Элемент 1</li>
+            <li>Элемент 1</li>
+            <li>Элемент 1</li>
+            <li>Элемент 1</li>
+            <li>Элемент 1</li>
+            <li>Элемент 1</li>
+        </ul>  
     </div> 
     );
     }
